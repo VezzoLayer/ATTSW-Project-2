cc703b1637810546bbc3a4ea1fda9591c62dd94e
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-22 17:35:26 +0200

    Creazione entità User

---
+++ src/main/java/com/ecommerce/manager/model/User.java
@@ -0,0 +1,90 @@
+package com.ecommerce.manager.model;
+
+import java.util.Objects;
+
+public class User {
+
+	private Long id;
+	private String username;
+	private String name;
+	private String email;
+	private long balance;
+	
+	public User() {
+		// required for serialization/deserialization
+	}
+	
+	public User(Long id, String username, String name, String email, long balance) {
+		this.id = id;
+		this.username = username;
+		this.name = name;
+		this.email = email;
+		this.balance = balance;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public long getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", name=" + name + ", email=" + email + ", balance="
+				+ balance + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(balance, email, id, name, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return balance == other.balance && Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name) && Objects.equals(username, other.username);
+	}
+	
+	
+}
