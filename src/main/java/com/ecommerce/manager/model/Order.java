0db8642a4c3f7203d10446dd44ee80f3c9516cf8
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-25 16:23:26 +0200

    test mapping jpa sulla classe User (green)

--- src/main/java/com/ecommerce/manager/model/Order.java
+++ src/main/java/com/ecommerce/manager/model/Order.java
@@ -8,8 +8,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "orders") // Plurale per evitare conflitti
 public class Order {
 
 	@Id
--- src/test/java/com/ecommerce/manager/jpa/EntitiesJpaTest.java
+++ src/test/java/com/ecommerce/manager/jpa/EntitiesJpaTest.java
@@ -1,5 +1,38 @@
 package com.ecommerce.manager.jpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ecommerce.manager.model.User;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
 public class EntitiesJpaTest {
 
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testUserJpaMapping() {
+		User savedUser = entityManager.persistFlushFind(new User(null, "username", "name", "email", 4000));
+
+		assertThat(savedUser.getUsername()).isEqualTo("username");
+		assertThat(savedUser.getName()).isEqualTo("name");
+		assertThat(savedUser.getEmail()).isEqualTo("email");
+		assertThat(savedUser.getBalance()).isEqualTo(4000);
+		assertThat(savedUser.getOrders()).isEmpty();
+
+		assertThat(savedUser.getId()).isNotNull();
+		assertThat(savedUser.getId()).isPositive();
+
+		// Per vedere identifier generato
+		LoggerFactory.getLogger(EntitiesJpaTest.class).info("Saved: {}", savedUser);
+	}
 }
