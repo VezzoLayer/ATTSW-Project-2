8b11d733aaad5145257372de40bd905a988153a0
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-25 17:22:03 +0200

    test sulla UserRepository che falliva, poi si implementa l'interfaccia che estende JpaRepository (green)

---
+++ src/main/java/com/ecommerce/manager/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.manager.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.manager.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
---
+++ src/test/java/com/ecommerce/manager/repositories/UserRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.ecommerce.manager.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ecommerce.manager.model.User;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testRepositoryContainsExactlyOneUser() {
+		User user = new User(null, "test", "test", "test", 4000);
+		User userSaved = entityManager.persistFlushFind(user);
+
+		Collection<User> users = repository.findAll();
+		assertThat(users).containsExactly(userSaved);
+	}
+}
