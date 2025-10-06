b8953cdb574e25ec98e1f5bfc156e0a3c1546ace
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-06 19:24:31 +0200

    Creazione classe  OrderRestControllerTest e poi classe OrderRestController tramite quick fix dove il Test ritorna tutti gli ordini quando non ci sono fallisce e implementazione chiamata (green)

---
+++ src/main/java/com/ecommerce/manager/controllers/OrderRestController.java
@@ -0,0 +1,27 @@
+package com.ecommerce.manager.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.manager.model.Order;
+import com.ecommerce.manager.services.OrderService;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderRestController {
+
+	private OrderService orderService;
+
+	public OrderRestController(OrderService orderService) {
+		this.orderService = orderService;
+	}
+
+	@GetMapping
+	public List<Order> allOrders() {
+		return orderService.getAllOrders();
+	}
+
+}
---
+++ src/test/java/com/ecommerce/manager/controllers/OrderRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.ecommerce.manager.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ecommerce.manager.services.OrderService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = OrderRestController.class)
+public class OrderRestControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@MockitoBean
+	private OrderService orderService;
+
+	@Test
+	public void testAllOrdersEmpty() throws Exception {
+		this.mvc.perform(get("/api/orders").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().json("[]"));
+	}
+}
