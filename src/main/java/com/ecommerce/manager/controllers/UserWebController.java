e66ab2a7a8018705c9af6d8dec65eb1cb32a7b48
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-08 19:30:05 +0200

    Test sulla home che mostra users e implementazione (green)

--- src/main/java/com/ecommerce/manager/controllers/UserWebController.java
+++ src/main/java/com/ecommerce/manager/controllers/UserWebController.java
@@ -1,13 +1,26 @@
 package com.ecommerce.manager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ecommerce.manager.services.UserService;
 
 @Controller
 public class UserWebController {
 
+	private static final String USERS_ATTRIBUTE = "users";
+
+	private UserService userService;
+
+	public UserWebController(UserService userService) {
+		this.userService = userService;
+	}
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute(USERS_ATTRIBUTE, userService.getAllUsers());
+
 		return "index";
 	}
 }
--- src/test/java/com/ecommerce/manager/controllers/UserWebControllerTest.java
+++ src/test/java/com/ecommerce/manager/controllers/UserWebControllerTest.java
@@ -1,19 +1,32 @@
 package com.ecommerce.manager.controllers;
 
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.ecommerce.manager.model.User;
+import com.ecommerce.manager.services.UserService;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = UserWebController.class)
 public class UserWebControllerTest {
+
+	@MockitoBean
+	private UserService userService;
 
 	@Autowired
 	private MockMvc mvc;
@@ -26,5 +39,14 @@ public class UserWebControllerTest {
 	@Test
 	public void testReturnHomeView() throws Exception {
 		ModelAndViewAssert.assertViewName(mvc.perform(get("/")).andReturn().getModelAndView(), "index");
+	}
+
+	@Test
+	public void testHomeViewShowsUsers() throws Exception {
+		List<User> users = asList(new User(1L, "test", "test", "test", 1000));
+
+		when(userService.getAllUsers()).thenReturn(users);
+
+		mvc.perform(get("/")).andExpect(view().name("index")).andExpect(model().attribute("users", users));
 	}
 }
