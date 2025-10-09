876515da26d0ed9ee19b20682b116e6ec8f91bcd
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-09 18:51:06 +0200

    Test sulla view Orders fa vedere orders quando ci sono e implementazione, creazione file orders.html (green)

--- src/main/java/com/ecommerce/manager/controllers/OrderWebController.java
+++ src/main/java/com/ecommerce/manager/controllers/OrderWebController.java
@@ -1,8 +1,31 @@
 package com.ecommerce.manager.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ecommerce.manager.model.Order;
+import com.ecommerce.manager.services.OrderService;
 
 @Controller
 public class OrderWebController {
+
+	private OrderService orderService;
+
+	public OrderWebController(OrderService orderService) {
+		this.orderService = orderService;
+	}
+
+	@GetMapping("/orders")
+	public String orders(Model model) {
+		List<Order> allOrders = orderService.getAllOrders();
+
+		model.addAttribute("orders", orderService.getAllOrders());
+		model.addAttribute("message", allOrders.isEmpty() ? "No order to show" : "");
+
+		return "orders";
+	}
 
 }
--- src/test/java/com/ecommerce/manager/controllers/OrderWebControllerTest.java
+++ src/test/java/com/ecommerce/manager/controllers/OrderWebControllerTest.java
@@ -1,11 +1,43 @@
 package com.ecommerce.manager.controllers;
 
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ecommerce.manager.model.Item;
+import com.ecommerce.manager.model.Order;
+import com.ecommerce.manager.model.User;
+import com.ecommerce.manager.services.OrderService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = OrderWebController.class)
 public class OrderWebControllerTest {
 
+	@MockitoBean
+	private OrderService orderService;
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void testOrdersViewShowsOrdersWhenThereAreOrders() throws Exception {
+		List<Order> orders = asList(new Order(1L, Item.BOX1, 700, new User(1L, "test", "test", "test", 1000)));
+
+		when(orderService.getAllOrders()).thenReturn(orders);
+
+		mvc.perform(get("/orders")).andExpect(view().name("orders")).andExpect(model().attribute("orders", orders))
+				.andExpect(model().attribute("message", ""));
+	}
 }
