284d7d0327dcb1de503d6642cca01ae959396f70
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-09 18:43:30 +0200

    Creazione OrderWebController e corrispettiva classe test

---
+++ src/main/java/com/ecommerce/manager/controllers/OrderWebController.java
@@ -0,0 +1,8 @@
+package com.ecommerce.manager.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderWebController {
+
+}
---
+++ src/test/java/com/ecommerce/manager/controllers/OrderWebControllerTest.java
@@ -0,0 +1,11 @@
+package com.ecommerce.manager.controllers;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = OrderWebController.class)
+public class OrderWebControllerTest {
+
+}
