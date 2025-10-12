f74c823391d67ce6fd2b0e25333b224ba3f1b586
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-13 00:30:44 +0200

    Test e implementazione home page nel body contiene No Users (green)

--- src/test/java/com/ecommerce/manager/controllers/UserWebControllerHtmlUnitTest.java
+++ src/test/java/com/ecommerce/manager/controllers/UserWebControllerHtmlUnitTest.java
@@ -1,6 +1,8 @@
 package com.ecommerce.manager.controllers;
 
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlPage;
@@ -26,7 +28,16 @@ public class UserWebControllerHtmlUnitTest {
 	@Test
 	public void testHomePageTitle() throws Exception {
 		HtmlPage page = webClient.getPage("/");
-		
+
 		assertThat(page.getTitleText()).isEqualTo("Users");
+	}
+
+	@Test
+	public void testHomePageWithNoUsers() throws Exception {
+		when(userService.getAllUsers()).thenReturn(emptyList());
+
+		HtmlPage page = this.webClient.getPage("/");
+
+		assertThat(page.getBody().getTextContent()).contains("No Users");
 	}
 }
