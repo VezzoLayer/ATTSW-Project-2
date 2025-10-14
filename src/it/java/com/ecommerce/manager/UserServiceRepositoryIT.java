e0dbb09af3d8b700858977a00bdb822d650f69da
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-14 16:42:45 +0200

    Creazione classe test per integration tests, configurazione pom per l'avvio automatico di docker prima degli it e chiusura dopo gli it, configurazione spring profile per l'utilizzo di postgresql, primo test che verifica che user service può inseire nella repo (green)

---
+++ src/it/java/com/ecommerce/manager/UserServiceRepositoryIT.java
@@ -0,0 +1,36 @@
+package com.ecommerce.manager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ecommerce.manager.model.User;
+import com.ecommerce.manager.repositories.UserRepository;
+import com.ecommerce.manager.services.UserService;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Import(UserService.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserServiceRepositoryIT {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	public void testServiceCanInsertIntoRepository() {
+		User savedUser = userService.insertNewUser(new User(null, "username", "name", "email", 2000));
+
+		assertThat(userRepository.findById(savedUser.getId())).isPresent();
+	}
+
+}
