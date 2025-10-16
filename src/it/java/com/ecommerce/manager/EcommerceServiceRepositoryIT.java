74e48bf5219db64cd920e208ef3616380595f207
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-16 15:49:26 +0200

    Ulteriori due tests di integrazione can insert order and update balance, can update user repo

--- src/it/java/com/ecommerce/manager/EcommerceServiceRepositoryIT.java
+++ src/it/java/com/ecommerce/manager/EcommerceServiceRepositoryIT.java
@@ -10,7 +10,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.ecommerce.manager.model.Item;
+import com.ecommerce.manager.model.Order;
 import com.ecommerce.manager.model.User;
+import com.ecommerce.manager.repositories.OrderRepository;
 import com.ecommerce.manager.repositories.UserRepository;
 import com.ecommerce.manager.services.EcommerceService;
 
@@ -26,10 +29,34 @@ public class EcommerceServiceRepositoryIT {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
+
 	@Test
 	public void testServiceCanInsertIntoUserRepository() {
 		User savedUser = ecommerceService.insertNewUser(new User(null, "username", "name", "email", 2000));
 
 		assertThat(userRepository.findById(savedUser.getId())).isPresent();
+	}
+
+	@Test
+	public void testServiceCanInsertIntoOrderRepositoryAndCorrectlyUpdateUserBalance() {
+		User user = ecommerceService.insertNewUser(new User(null, "u1", "n1", "e1", 2000));
+		Order order = new Order(null, Item.BOX1, 500, user);
+
+		Order savedOrder = ecommerceService.insertNewOrder(order);
+
+		assertThat(orderRepository.findById(savedOrder.getId())).isPresent();
+		assertThat(ecommerceService.getUserById(user.getId()).getBalance()).isEqualTo(1500);
+	}
+
+	@Test
+	public void testServiceCanUpdateUserRepository() {
+		User savedUser = ecommerceService.insertNewUser(new User(null, "username", "name", "email", 1000));
+
+		User modifiedUser = ecommerceService.updateUserById(savedUser.getId(),
+				new User(savedUser.getId(), "mod username", "mod name", "mod email", 2000));
+
+		assertThat(userRepository.findById(savedUser.getId())).contains(modifiedUser);
 	}
 }
