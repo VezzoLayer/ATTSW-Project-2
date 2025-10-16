c5d16f815c5bb59fd708b331799be2bccd454d75
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-16 15:54:00 +0200

    Ulteriori tests di integrazione, su withdraw, balance, findUser minori di un certo balance

--- src/it/java/com/ecommerce/manager/EcommerceServiceRepositoryIT.java
+++ src/it/java/com/ecommerce/manager/EcommerceServiceRepositoryIT.java
@@ -2,6 +2,8 @@ package com.ecommerce.manager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,5 +60,35 @@ public class EcommerceServiceRepositoryIT {
 				new User(savedUser.getId(), "mod username", "mod name", "mod email", 2000));
 
 		assertThat(userRepository.findById(savedUser.getId())).contains(modifiedUser);
+	}
+
+	@Test
+	public void testRepositoryCorrectlyFindsUserWithLowBalance() {
+		User userShouldBeFound1 = ecommerceService.insertNewUser(new User(null, "u1", "n1", "e1", 800));
+		User userShouldBeFound2 = ecommerceService.insertNewUser(new User(null, "u2", "n2", "e2", 500));
+
+		ecommerceService.insertNewUser(new User(null, "not", "not", "not", 1500));
+
+		List<User> users = userRepository.findAllUsersWithLowBalance(1000);
+
+		assertThat(users).containsExactly(userShouldBeFound1, userShouldBeFound2);
+	}
+
+	@Test
+	public void testServiceCanCorrectlyDepositIntoUserRepository() {
+		User savedUser = ecommerceService.insertNewUser(new User(null, "username", "name", "email", 1000));
+
+		ecommerceService.deposit(savedUser.getId(), 1000);
+
+		assertThat(userRepository.findById(savedUser.getId()).orElseThrow().getBalance()).isEqualTo(2000);
+	}
+	
+	@Test
+	public void testServiceCanCorrectlyWithdrawIntoUserRepository() {
+		User savedUser = ecommerceService.insertNewUser(new User(null, "username", "name", "email", 1000));
+
+		ecommerceService.withdraw(savedUser.getId(), 500);
+
+		assertThat(userRepository.findById(savedUser.getId()).orElseThrow().getBalance()).isEqualTo(500);
 	}
 }
