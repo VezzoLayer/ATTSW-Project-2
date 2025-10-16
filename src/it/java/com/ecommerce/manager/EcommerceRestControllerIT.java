6f2fba0d4c46773f71d9ac0e550ec9b7c7923838
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-10-16 22:55:31 +0200

    restcontrollerIT test sull'updateUser e su newOrder

--- src/it/java/com/ecommerce/manager/EcommerceRestControllerIT.java
+++ src/it/java/com/ecommerce/manager/EcommerceRestControllerIT.java
@@ -2,6 +2,7 @@ package com.ecommerce.manager;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,7 +14,9 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.ecommerce.manager.model.Item;
 import com.ecommerce.manager.model.User;
+import com.ecommerce.manager.model.Order;
 import com.ecommerce.manager.repositories.OrderRepository;
 import com.ecommerce.manager.repositories.UserRepository;
 
@@ -52,5 +55,30 @@ public class EcommerceRestControllerIT {
 		User savedUser = response.getBody().as(User.class);
 
 		assertThat(userRepository.findById(savedUser.getId())).contains(savedUser);
+	}
+
+	@Test
+	public void testUpdateUser() {
+		User savedUser = userRepository.save(new User(null, "og username", "og name", "og email", 1000));
+
+		given().contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(new User(null, "mod username", "mod name", "mod email", 2000)).when()
+				.put("/api/users/update/" + savedUser.getId()).then().statusCode(200).body("id",
+						equalTo(savedUser.getId().intValue()), "username", equalTo("mod username"), "name",
+						equalTo("mod name"), "email", equalTo("mod email"), "balance", equalTo(2000));
+	}
+
+	@Test
+	public void testNewOrder() {
+		User savedUser = new User(null, "username", "name", "email", 1000);
+		userRepository.save(savedUser);
+
+		Response response = given().contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(new Order(null, Item.BOX1, 500, savedUser)).when().post("/api/orders/new");
+
+		Order savedOrder = response.getBody().as(Order.class);
+
+		assertThat(orderRepository.findById(savedOrder.getId())).contains(savedOrder);
+		assertThat(userRepository.findById(savedUser.getId()).orElseThrow().getBalance()).isEqualTo(500);
 	}
 }
